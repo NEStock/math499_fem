443042c7cac495f1bc2253161a112b85e9ac482b
Nicole Stock <nicoleelizabeth888@gmail.com>
2020-09-09 16:49:48 -0400

    Adding function explanation

--- main_no_exact.m
+++ main_no_exact.m
@@ -1,6 +1,41 @@
-function [err,grad_err,max_err] = main(f,gd,sf,ns,mesh_level,opt)
+function [err,grad_err,max_err] = main_no_exact(u,gd,sf,ns,mesh_level,n)
 %MAIN Summary of this function goes here
 %   Detailed explanation goes here
+%
+% Syntax:
+%     [err,grad_err,max_err] = 
+%       main_no_exact(u,gd,sf,ns,mesh_level, n)
+%
+% Inputs:
+%     u - given function
+%     gd,sf,ns - outputs of pdepoly specifying domain
+%     mesh_level - max mesh level
+%     n - Hodge Laplacian on Axisymmetrix Domain and its Discretization
+%     weight
+%
+% Outputs:
+%     err - array of L2 errors for mesh levels corresponding to indices
+%     grad_err - array of L2 gradient errors for mesh levels  
+%         corresponding to indices
+%     max_err - array of max errors for mesh levels corresponding to
+%         indicies
+%
+% Usage Exampled:
+%    u = @(r,z) r^(1/2);
+%    mesh = 8;
+%    n = 1;
+%    pdepoly([0,1,1,0], [-1,-1,1,1]);
+%    [err,grad_err,max_err] = 
+%       main_no_exact(u,gd,sf,ns,mesh_level, n)
+% Dependencies:
+%    basis_functions_weighted_p2.m
+%    display_errors.m
+%    errors_no_exact_weighted_p2.m
+%    prolongation_matrix.m
+%    stiffness_matrix_weighted_p2.m
+%
+% Note:
+%    prolongation_matrix.m not working yet. Thus errors are wrong.
 %
 % Author: Nicole Stock
 % Date: Fall 2020
@@ -26,7 +61,7 @@ if mesh_level > 1
 
     [p2,t2] = find_midpoints(p,t);
 
-    [basis,Qh] = solve(p,p2,e,t,t2,f);
+    [basis,Qh] = solve(p,p2,e,t,t2,u,n);
     % errors_no_exact_weighted_p2(p,t,p2,t2,basis,u_h_km1,u_h_k,k)
 
     for i = 2:mesh_level
@@ -44,14 +79,14 @@ if mesh_level > 1
         [p2,t2] = find_midpoints(p,t);
 
         % Create prolongation matrix to extend mesh level - 1 vectors
-        P = prolongation_matrix(nodes_1,mid_nodes_1,p,p2,t_1,t2_1,basis_1,k);
+        P = prolongation_matrix(nodes_1,mid_nodes_1,p,p2,t_1,t2_1,basis_1,n);
 
         % Extend Qh
         Qh_extended = P*Qh_1;
         
         % Solve
-        [basis,Qh] = solve(p,p2,e,t,t2,f);
-        [err(i),grad_err(i),max_err(i)] = errors_no_exact_weighted_p2(p,t,p2,t2,basis,Qh_extended,Qh,k);
+        [basis,Qh] = solve(p,p2,e,t,t2,u,n);
+        [err(i),grad_err(i),max_err(i)] = errors_no_exact_weighted_p2(p,t,p2,t2,basis,Qh_extended,Qh,n);
     end
     
     display_errors(err,grad_err,max_err)
@@ -62,9 +97,9 @@ end
 end
 
 % subfunction
-function [basis,Qh] = solve(p,p2,e,t,t2,u)
+function [basis,Qh] = solve(p,p2,e,t,t2,u,n)
     basis = basis_functions_weighted_p2(p,t,p2,t2);
-    S = stiffness_matrix_weighted_p2(p,t,p2,t2,basis);
+    S = stiffness_matrix_weighted_p2(p,t,p2,t2,basis,n);
     b = create_b_p2(p,t,p2,t2,basis,u);
     Qh = S\b;
 

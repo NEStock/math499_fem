923ea9674abe18cfcec36d31032101d871d76ae8
Nicole Stock <nicoleelizabeth888@gmail.com>
2020-12-10 18:50:59 -0500

    Correcting exact solutions and input F for k=2 case

--- data/weighted_HL_k_2/get_data_1.m
+++ data/weighted_HL_k_2/get_data_1.m
@@ -11,13 +11,13 @@ function [u_vec_r,u_vec_th,u_vec_z,s_vec_r,s_vec_th,s_vec_z,f_vec_r,f_vec_th,f_v
 %         -3n^2 + (2n^2)/r - 6n + (6n)/r + 9
 %         0      ]
 
-u_vec_r = @(r,z) 3.*r.*(r-1); % 3.*r.^2 - 3.*r
-u_vec_th = @(r,z) -3.*r.^2 + 2.*r;
-u_vec_z = @(r,z) 0;
-s_vec_r = @(r,z) 0;
-s_vec_th = @(r,z) 0;
-s_vec_z = @(r,z) (r.^2).*(r-1); % r.^3 - r.^2
-f_vec_r = @(r,z) 3.*n.^2 - 3.*(n.^2)./r + 6.*n - 4.*n./r - 9;
-f_vec_th = @(r,z) -3.*n.^2 + 2.*(n.^2)./r - 6.*n + 6.*n./r + 9;
-f_vec_z = @(r,z) 0;
+s_vec_r = @(r,z) 3.*r.*(r-1); % 3.*r.^2 - 3.*r
+s_vec_th = @(r,z) -3.*r.^2 + 2.*r;
+s_vec_z = @(r,z) 0;
+u_vec_r = @(r,z) 0;
+u_vec_th = @(r,z) 0;
+u_vec_z = @(r,z) (r.^2).*(r-1); % r.^3 - r.^2
+f_vec_r = @(r,z) 0;
+f_vec_th = @(r,z) 0;
+f_vec_z = @(r,z) (n.^2).*r - (n.^2) - 9.*r + 4;
 end

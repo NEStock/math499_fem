6b09ee9441db8d2bdafa5664ba3f901aa0b5ee1e
Nicole Stock <stockne@dukes.jmu.edu>
2021-03-01 11:23:50 -0500

    Fixing k=2 data

--- k_2/data/get_data_1.m
+++ k_2/data/get_data_1.m
@@ -1,19 +1,19 @@
 function [u_vec_r,u_vec_th,u_vec_z,s_vec_r,s_vec_th,s_vec_z,f_vec_r,f_vec_th,f_vec_z] = get_data_1(n)
 %GET_DATA1 Get u vector and s equation exact solution
 %   data1
-%   u = [ 3r(r-1)
+%   u = [ nr(r-1)
 %         -3r^2 + 2r
 %         0          ]
 %   s = [ 0
 %         0
 %         r^2(r-1) ]
-%   f = [ 3n^2 - (3n^2)/r + 6n - (4n)/r - 9
-%         -3n^2 + (2n^2)/r - 6n + (6n)/r + 9
-%         0      ]
+%   f = [ 0
+%         0
+%         rn^2 - n^2 - 9r + 4 ]
 % Author: Nicole Stock
 % Date: Fall 2020
 
-s_vec_r = @(r,z) 3.*r.*(r-1); % 3.*r.^2 - 3.*r
+s_vec_r = @(r,z) n.*r.*(r-1); % 3.*r.^2 - 3.*r
 s_vec_th = @(r,z) -3.*r.^2 + 2.*r;
 s_vec_z = @(r,z) 0;
 u_vec_r = @(r,z) 0;
